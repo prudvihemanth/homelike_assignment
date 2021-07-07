c667712434a414d54fc7348b4d3749b52dcb3312
pruanki <prudvihemanth@gmail.com>
2021-07-07 18:58:35 +0530

    added logger

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Home Like Assignment:
 -- Tech Stack (Node Js (Hapi js with Typescript), MongoDb)
 -- Features Implemented (User Registration, Authentication, Autherisation, Create Apartment, Search Apartment, Mark Favourite apartment, List all apartments)
 --Best Practices (logging, docker compose, testing, tsdocs, swagger)
+
+-- node version: v14.17.3 / npm version v14.17.3
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Server, Request, ResponseToolkit } from "@hapi/hapi";
+import Logger from "./src/utils/logger"
 
 const init = async () => {
     const server: Server = new Server({
@@ -9,6 +10,11 @@ const init = async () => {
         method: 'GET',
         path: '/',
         handler: (request: Request, h: ResponseToolkit) => {
+            Logger.error("This is an error log");
+            Logger.warn("This is a warn log");
+            Logger.info("This is a info log");
+            Logger.http("This is a http log");
+            Logger.debug("This is a debug log");
             return 'Hello World!';
         }
     });
