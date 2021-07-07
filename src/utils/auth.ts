114bab0698aba97fd868d66ee9dc17ff3271d2e7
pruanki <prudvihemanth@gmail.com>
2021-07-07 23:50:52 +0530

    added typescript docs integration

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,25 @@
 //const token = JWT.sign(people[1], secret); // synchronous
 
-// const validate = async function (decoded, request, h) {
+ /**
+   * Returns the average of two numbers.
+   *
+   * @remarks
+   * This method is part of the {@link core-library#Statistics | Statistics subsystem}.
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The arithmetic mean of `x` and `y`
+   *
+   * @beta
+   */
+
+const validate =  function () {
  
-//     // do your checks to see if the person is valid
-//     if (!people[decoded.id]) {
-//       return { isValid: false };
-//     }
-//     else {
-//       return { isValid: true };
-//     }
-// };
+    // do your checks to see if the person is valid
+    if (true) {
+      return { isValid: false };
+    }
+    else {
+      return { isValid: true };
+    }
+};
